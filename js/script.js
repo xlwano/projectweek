66890d989a84065874424b6485cb46010a9e9415
Lano <1217628@student.roc-nijmegen.nl>
2025-11-12 14:06:09 +0100

    searchbar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const form = document.getElementById('searchForm');
 const input = document.getElementById('searchInput');
 
+console.log(form);
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   const query = input.value.trim();
+  console.log('Search query:', query);
   if (query) {
     // For example, search on Google for the query:
     const url = 'https://www.google.com/search?q=' + encodeURIComponent(query);
